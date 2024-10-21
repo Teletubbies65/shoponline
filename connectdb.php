61c48ccd932810bfdff05d28ce8678d7b3940248
Teletubbies65 <Simatt.bc@gmail.com>
2024-10-21 09:21:22 +0700

    99

--- connectdb.php
+++ connectdb.php
@@ -2,7 +2,7 @@
 <?php
 $host = "localhost";
 $usr = "root";
-$pwd = "12345678";
+$pwd = "";
 $dbName = "shoponline2";
 
 $conn = mysqli_connect($host, $usr, $pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้") ;
