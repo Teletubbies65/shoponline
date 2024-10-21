e09004fad731675b1163c92cf4243a7cd02f689d
Teletubbies65 <Simatt.bc@gmail.com>
2024-10-21 09:07:04 +0700

    00

--- e/album/checklogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['aid'])) {
-  echo '<p style="color:red;">' . htmlspecialchars('Access Denied!', ENT_QUOTES, 'UTF-8') . '</p>';
-  header('Refresh: 3; URL=../sign-in/index.php');
-  exit;
-}
-?>
--- e/album/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	unset($_SESSION['aid']);
-	unset($_SESSION['aname']);
-	echo "<script>";
-				echo "window.location='../sign-in/index.php';";
-				echo "</script>";
-?>
